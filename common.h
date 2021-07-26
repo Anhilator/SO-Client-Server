00269428cd70b3b107e151371e446bc788f361db
anhilator <guidosamuele.derosa3@gmail.com>
2021-07-26 21:45:51 +0200

    Update login

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 #define handle_error(msg)           do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 /* Configuration parameters */
-#define DEBUG           1   // display debug messages
+#define DEBUG           0   // display debug messages
 #define MAX_CONN_QUEUE  3   // max number of connections the server can queue
 #define SERVER_ADDRESS  "127.0.0.1"
 #define SERVER_COMMAND  "QUIT\n"
