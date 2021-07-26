9cdcba002da37e660928954ce586819bee1cfc15
anhilator <guidosamuele.derosa3@gmail.com>
2021-07-26 15:38:05 +0200

    Prima prova di login

--- client.c
+++ client.c
@@ -9,77 +9,30 @@
 
 #include "common.h"
 
-int main(int argc, char* argv[]) {
-    int ret,bytes_sent,recv_bytes;
-
-    // variables for handling a socket
-    int socket_desc;
-    struct sockaddr_in server_addr = {0}; // some fields are required to be filled with 0
-
-    /** [SOLUTION]
-     *  TODO: create a socket for contacting the server
-     *
-     * Suggestions:
-     * - protocollo AF_INET
-     * - tipo SOCK_STREAM
-     */
-    socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socket_desc < 0)
-        handle_error("Could not create socket");
-
-    if (DEBUG) fprintf(stderr, "Socket created...\n");
-
-    /** [SOLUTION]
-     *  TODO: set up parameters for the connection and initiate a connection to the server
-     *
-     * Suggestions:
-     * - set the 3 fields of server:
-     * - - server_addr.sin_addr.s_addr: we must specify the server address
-     * - - server_addr.sin_family,
-     * - - server_addr.sin_port (using htons() method)
-     * - initiate a connection to the server
-     * - - attention to the bind method:
-     * - - it requires as second field struct sockaddr* addr, but our address is a struct sockaddr_in, hence we must cast it (struct sockaddr*) &server_addr
-     */
-    //
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
-    server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(SERVER_PORT); // don't forget about network byte order!
-
-
+void login(int socket_desc, struct sockaddr_in server_addr){
     char buf[1024];
     size_t buf_len = sizeof(buf);
     int msg_len;
-    memset(buf,0,buf_len);
+    memset(buf,0,buf_len);      
 
-    // main loop
+    int ret, bytes_sent,recv_bytes;
+
+    // sequenza di login
     while (1) {
         char* quit_command = SERVER_COMMAND;
         size_t quit_command_len = strlen(quit_command);
 
-        printf("Insert your message: ");
+        printf("Enter username: ");
 
-        /* Read a line from stdin
-         *
-         * fgets() reads up to sizeof(buf)-1 bytes and on success
-         * returns the first argument passed to it. */
+        //Leggi una line da stdin
         if (fgets(buf, sizeof(buf), stdin) != (char*)buf) {
             fprintf(stderr, "Error while reading from stdin, exiting...\n");
             exit(EXIT_FAILURE);
         }
 
         msg_len = strlen(buf);
-//        buf[--msg_len] = '\0'; // remove '\n' from the end of the message
 
-		/** [SOLUTION]
-         *  TODO: send message to server
-         *
-         * Suggestions:
-         * - send() with flags = 0 is equivalent to write() to a descriptor
-         * - the number of bytes to send is msg_len as we have discarded '\n'
-         * - deal with partially sent messages
-         * - message size IS NOT buf size
-         */
+		// la mando al server
         bytes_sent=0;
         while ( bytes_sent < msg_len) {
             ret = sendto(socket_desc, buf, msg_len, 0, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
@@ -88,38 +41,34 @@ int main(int argc, char* argv[]) {
             bytes_sent = ret;
         }
 
-        if (DEBUG) fprintf(stderr, "Sent message of %d bytes...\n", bytes_sent);
+        printf("Enter password: ");
 
+        // idem
+        if (fgets(buf, sizeof(buf), stdin) != (char*)buf) {
+            fprintf(stderr, "Error while reading from stdin, exiting...\n");
+            exit(EXIT_FAILURE);
+        }
 
-        /** [SOLUTION]
-         *  TODO: After a quit command we won't receive any more data from
-         *  the server, thus we must exit the main loop.
-         *
-         * Suggestions:
-         * - compare the number of bytes sent with the length of the
-         *   quit command that tells the server to close the connection
-         *   (see SERVER_COMMAND macro in common.h)
-         * - perform a byte-to-byte comparsion when required using
-         *   memcmp(const void *ptr1, const void *ptr2, size_t num)
-         * - exit from the cycle when there is nothing left to receive
-         */
+        msg_len = strlen(buf);
+
+        // idem
+        bytes_sent=0;
+        while ( bytes_sent < msg_len) {
+            ret = sendto(socket_desc, buf, msg_len, 0, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
+            if (ret == -1 && errno == EINTR) continue;
+            if (ret == -1) handle_error("Cannot write to the socket");
+            bytes_sent = ret;
+        }
+
+        // può tornare utile più avanti
 		if (msg_len == quit_command_len && !memcmp(buf, quit_command, quit_command_len)){
 
             if (DEBUG) fprintf(stderr, "Sent QUIT command ...\n");
              break;
         }
 
-        /** [SOLUTION]
-         *  TODO: read message from server
-         * Suggestions:
-         * - recv() with flags = 0 is equivalent to read() from a descriptor
-         * - for sockets, we get a 0 return value only when the peer closes
-         *   the connection: if there are no bytes to read and we invoke
-         *   recv() we will get stuck, because the call is blocking!
-         * - deal with partially sent messages (we do not know the message size)
-         */
-
-	recv_bytes = 0;
+        // ricevo la risposta dal server
+	    recv_bytes = 0;
     	do {
             ret = recvfrom(socket_desc, buf, buf_len, 0, NULL, NULL);
             if (ret == -1 && errno == EINTR) continue;
@@ -131,14 +80,16 @@ int main(int argc, char* argv[]) {
 
         if (DEBUG) fprintf(stderr, "Received answer of %d bytes...\n",recv_bytes);
 
-        printf("Server response: %s\n", buf); // no need to insert '\0'
+        printf("Server response: %s\n", buf); 
+
+        // Se il server mi risponde positivamente esco dal ciclo in quanto mi sono loggato
+        if(strcmp(buf, "Login effettuato con successo")==0) return;
     }
+}
 
-
-    /** [SOLUTION]
-     *  TODO: close socket and release unused resources
-     */
-    ret = close(socket_desc);
+void disconnect(int socket_desc){
+    // chiudo le varie socket
+    int ret = close(socket_desc);
     if (ret < 0) handle_error("Cannot close the socket");
 
     if (DEBUG) fprintf(stderr, "Socket closed...\n");
@@ -146,4 +97,61 @@ int main(int argc, char* argv[]) {
     if (DEBUG) fprintf(stderr, "Exiting...\n");
 
     exit(EXIT_SUCCESS);
+
+}
+
+void printTitle(){
+    //char ch, username[20] = {}, password[20] = {};
+
+    
+    char *filename = "title_client.txt";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        handle_error("Errore apertura file\n");
+    }
+
+    // reading line by line, max 256 bytes
+    const unsigned MAX_LENGTH = 256;
+    char buffer[MAX_LENGTH];
+
+    while (fgets(buffer, MAX_LENGTH, fp))
+        printf("%s", buffer);
+
+    // close the file
+    fclose(fp);
+
+}
+
+int main(int argc, char* argv[]) {
+    system("clear");
+    printTitle();
+    
+    
+
+    // descrittore socket 
+    int socket_desc;
+    struct sockaddr_in server_addr = {0}; 
+
+    // inizializzo la socket di tipo SOCK_DGRAM, ovvero UDP
+    socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket_desc < 0)
+        handle_error("Could not create socket");
+
+    if (DEBUG) fprintf(stderr, "Socket created...\n");
+
+    
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);   // setto l'indirizzo
+    server_addr.sin_family      = AF_INET;                     // scelgo il protocollo Ipv4 
+    server_addr.sin_port        = htons(SERVER_PORT);          // setto il byte order
+
+
+    
+
+    login(socket_desc, server_addr);
+
+    printf("Loggato");
+    while(1);
+    
 }
