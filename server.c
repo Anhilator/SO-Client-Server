69cd23cee23f7a2be2e6398dfdd66268d5e3b664
anhilator <guidosamuele.derosa3@gmail.com>
2021-07-26 15:36:34 +0200

    Prima prova di login

--- server.c
+++ server.c
@@ -89,6 +89,132 @@ void printTitle(){
 
 }
 
+int file_authentication(char * username, char * password){
+    FILE * fl;
+    long fl_size;
+    char * buffer;
+    size_t res;
+
+    //estraggo il carattere \n da username e password
+    strtok(username, "\n"); 
+    strtok(password, "\n");
+        
+    fl = fopen ( "user.txt" , "r+" );
+    if (fl==NULL) {
+        handle_error("Errore apertura del file \n");
+    }
+
+    fseek (fl , 0 , SEEK_END);
+    fl_size = ftell (fl);
+    rewind (fl);
+
+    buffer = (char*) malloc (sizeof(char)*fl_size);
+    
+
+    res = fread (buffer,1,fl_size,fl);
+    if (res != fl_size) {
+        handle_error("Errore in lettura file");
+    }
+
+    char * strtok_res;
+    strtok_res = strtok(buffer, "::");
+    int i=0, check = 0;
+    while (strtok_res != NULL)
+    {
+        if (DEBUG) printf ("%s \n", strtok_res);
+       
+        if(i==0 && strcmp(strtok_res, username) == 0) {check++; i++;}
+        else {
+            if(check == 1 && i==1 && (strcmp(strtok_res, password) == 0)) return 1;
+            else {check = 0; i=0;}
+        } 
+        
+        strtok_res = strtok (NULL, "::\n");
+    }
+
+    fclose (fl);
+    free (buffer);
+    return 0;
+}
+
+void authentication(int socket_desc){
+    //printf("sd: %d \n", socket_desc);
+    int ret, recv_bytes, bytes_sent;
+    char username[1024], password[1024];
+    size_t user_len = sizeof(username);
+    size_t pass_len = sizeof(password);
+
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    char buf[1024]; // buffer in cui andrò a scrivere il messaggio
+    size_t buf_len = sizeof(buf);
+    //int msg_len; 
+    memset(buf,0,buf_len); //setto il buffer tutto a 0
+
+    char* quit_command = SERVER_COMMAND; //comando per terminare la connessione
+    size_t quit_command_len = strlen(quit_command);
+
+    struct sockaddr_in client_addr;
+    int sockaddr_len = sizeof(client_addr); 
+
+    // echo loop
+    while (1) {
+        
+        recv_bytes = 0;
+        do {
+            // ricevo i byte comunicati dal client sul suo username 
+            recv_bytes = recvfrom(socket_desc, username, user_len, 0,(struct sockaddr*) &client_addr, (socklen_t*) &sockaddr_len);
+            if (recv_bytes == -1 && errno == EINTR) continue; 
+            if (recv_bytes == -1) handle_error("Cannot read from the socket");
+            if (recv_bytes == 0) break;
+		} while ( recv_bytes <= 0 ); // devo evitare la possibilità di messaggi letti parzialmente
+
+        recv_bytes = 0;
+        do {
+            // ricevo i byte comunicati dal client sulla sua password 
+            recv_bytes = recvfrom(socket_desc, password, pass_len, 0,(struct sockaddr*) &client_addr, (socklen_t*) &sockaddr_len);
+            if (recv_bytes == -1 && errno == EINTR) continue; 
+            if (recv_bytes == -1) handle_error("Cannot read from the socket");
+            if (recv_bytes == 0) break;
+		} while ( recv_bytes <= 0 ); 
+        
+        
+
+       
+
+        //verifico se l'utente ha inserito dati corretti
+        char msg[1024];
+        if(file_authentication(username, password)){
+            sprintf(msg, "Login effettuato con successo");
+            time ( &rawtime );
+            timeinfo = localtime ( &rawtime );
+            printf("%s si è loggato %s \n", username, asctime(timeinfo));  
+            
+        }else{
+            sprintf(msg, "Dati errati, riprovare");
+        }
+        size_t msg_len = sizeof(msg);
+
+        
+              
+        // mando il responso del login
+        bytes_sent=0;
+        while ( bytes_sent < recv_bytes) {
+            ret = sendto(socket_desc, msg, msg_len, 0,(struct sockaddr*) &client_addr, sockaddr_len);
+            if (ret == -1 && errno == EINTR) continue;
+            if (ret == -1) handle_error("Cannot write to the socket");
+            bytes_sent += ret;
+        }
+        break;
+        
+
+    }
+    return;
+}
+
+
+
 int main(int argc, char* argv[]) {
 
     system("clear"); 
